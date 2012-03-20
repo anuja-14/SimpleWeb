03091a659d7f24af82e2f82065d889517e9854f1
CS09B046 <avaibhav@gokulan.cse.iitm.ac.in>
2012-03-20 09:52:19 +0530

    Images in text widget found . Maximized window

--- text_box.py
+++ text_box.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 
+# Writes content at the current end of the file . So we could just use this to print data.
 def writecontent(msg ,log ):
     numlines = log.index('end - 1 line').split(' . ')[0]
     log['state']='normal'
@@ -8,12 +9,21 @@ def writecontent(msg ,log ):
     log.insert('end' , msg)
     log['state']='disabled'
 
+# Start of the code to display the browser
 root = Tk()
-w = Text ( root , bg = "yellow" , wrap = WORD  );
+root.geometry('%sx%s+0+0'% root.maxsize())
+w = Text ( root , bg = "yellow" , wrap = WORD  );           # text widget created 
 w.grid()
-f = open('text_box.py' , 'r' )
+w.pack(side=LEFT , fill = BOTH , expand = YES )
+f = open('text_box.py' , 'r' )          
+img = PhotoImage ( file = 'Sachin_Ramesh_Tendulkar.gif' )           # Init the image to be put in the page and gets it ready :)
+count = 0
 for line in f :
     writecontent(line , w );
+    count = count +1;
+    if ( count == 20 ):
+        w.image_create( INSERT ,   image = img )                    # insert the picture at the current INSERT position.
+ 
 root.mainloop()
 
 # Comments on the code of Tkinter : This is a simple code for just showing the file on a window . Read up more on Tkinter and understand the obove code . There are some nice references online . The main thing that we would need to do now , is to look for how to format the output based on some tag . 
